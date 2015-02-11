878188e7e4c0880553451c62cdb60dd92b2022eb
EllieSherrardSmith <e.sherrard-smith@imperial.ac.uk>
2015-02-11 11:20:42 +0000

    update

--- dataFormat.r
+++ dataFormat.r
@@ -1,3 +1,49 @@
+#' @description
+#' This function converts an \linkS4class{obkData} object to a json object storing outbreak data.
+#'
+#'
+#'
+#'
+##################
+## obkData2json ##
+##################
+obkData2json <- function(x){
+  ## CHECK PRESENCE OF INDIVIDUAL INFORMATION ##
+  if(){
+    ## generate empty object ##
+    warning("No individual information - returning empty object")
+    return()
+  }
+  
+  ## SHAPE DATA INTO LIST: ##
+  ## $indiv1:
+  ## $id
+  ## $attributes
+  ## $name1 $type $value
+  ## $name2
+  ## $name3
+  ## $records
+  ## $record1
+  ## $id
+  ## $name
+  ## $date
+  ## $location
+  ## $attributes
+  ## $attribute1
+  ## $attribute2
+  ## $record2
+  ## $id
+  ## $name
+  ## $date
+  ## $location
+  ## $attributes
+  ## $attribute1
+  ## $attribute2
+  ## $attribute3
+  
+  ##Example below
+
+
 { 
   metadata: [
       {"name": "owner", "type": "string", "value": "hackout2 team"},
@@ -145,5 +191,21 @@
                       "attributes":{"name":"outcome", "type":"int", "value":"died"}
                     }
                   ]
-        }
+        } 
 ]
+
+## AUXIL FUNCTION TO PROCESS ONE INDIVIDUAL ##
+## x: obkData
+## id: individual id
+getInfoOneIndiv <- function(x, id){
+  ## get meta info on individual ##
+  ## get list of records for this individual ##
+  ## build output list ##
+  ## return result ##
+}
+## AUXIL FUNCTION TO GENERATE ATTRIBUTES FROM NAMED VALUES ##
+f1 <- function(x) toJSON(list(name=names(x), type=typeof(x), value=x))
+## PROCESS INDIVIDUALS ##
+## GET LIST TO BE CONVERTED ##
+## CONVERT TO JSON ##
+}
